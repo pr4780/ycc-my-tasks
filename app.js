1457f693c0925099379f16880878ce60f8405410
German Grachev <reGrachDev@yandex.ru>
2025-04-29 18:50:22 +0300

    update repo

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 
-
 function crud() {
-    // операция чтения элемента
     const elem = document.getElementById('main_head');
     console.log(elem);
 
     setTimeout(() => {
-        elem.innerText = 'Это совсем не смешные делишки'
+        elem.innerText = 'Ох уж эти делишки!'
     }, 2000);
 
     setTimeout(() => {
-        elem.parentNode.removeChild(elem)
-    }, 4000)
+        elem.parentNode.removeChild(elem);
+    }, 4000);
 
     setTimeout(() => {
         const newElem = document.createElement('h1');
-        newElem.innerText = 'Деловые дела'
+        newElem.innerText = 'Новое начало. Ура!'
+        newElem.id = 'main_head';
         document.body.prepend(newElem);
-    }, 6000)
+    }, 6000);
 }
 
-const btn = document.getElementById('main_head');
-btn.onclick = crud;
+
+function addNewTask() {
+    // 1. Получить инпут и текст в нем
+    const elem_input = document.getElementById('new_task');
+    const task_name = elem_input.value;
+
+    // 2. Создать новый элемент li
+    const elem_li = document.createElement('li');
+    elem_li.innerText = task_name;
+
+    // 3. Вставить новый элемент в начало списка
+    const elem_ul = document.getElementById('task_list');
+    elem_ul.prepend(elem_li);
+}
+
+const btn = document.getElementById('btn_click');
+btn.onclick = addNewTask;
+
